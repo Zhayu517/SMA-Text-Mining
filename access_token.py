d70c45d0577525e6d3131b802dd4286c8fb12b8a
Zhayu517 <zhangzy517@gmail.com>
2022-03-17 21:43:27 +0000

    try using client rather than api

--- access_token.py
+++ access_token.py
@@ -2,3 +2,11 @@ api_token = "jXMA8AEL8KW4QBQpJZw69Mh77"
 api_token_secret = "uD6xTzrqVU26M0MS7lG9s81FRgHUu5gHzDBhPp9Nf3goFObe19"
 access_token = "1324002361694654465-8LZDKWBXdb6ac1J30r6SsRrfJj5Fh1"
 access_token_secret = "PQKX7cInfMhrGi9mL63PlABxhdQlzXEMY1URBzQMxxiOk"
+bearer_token = "AAAAAAAAAAAAAAAAAAAAADBgaQEAAAAAdpWH1Wc%2BG32Sj%2FZzrKX4U3jhkCI%3D7yHS67VXIxTOj1Db5lJB1yg6AB9i1FWeS0owPW0ZjofBsJ5lGd"
+
+tokens = {}
+tokens['api_token'] = api_token
+tokens['api_token_secret'] = api_token_secret
+tokens['access_token'] = access_token
+tokens['access_token_secret'] = access_token_secret
+tokens['bearer_token'] = bearer_token
